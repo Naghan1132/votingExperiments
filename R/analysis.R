ba168c380e68883c4df1972e7e154d8dc4091315
Naghan1132 <grimault52@gmail.com>
2023-06-02 11:51:00 +0200

    un nb de candidat fixe pour chaque sim

--- R/analysis.R
+++ R/analysis.R
@@ -1,115 +1,130 @@
-# === Analysis ===
-# ACM
-# CAH
-# MDS
-
-# ====
-
-# clustering entre les méthodes (en excluant les autres colonnes)
-# ACM sur toute les variables ?
-# mettre en facteur les resultats de méthodes ????
-
-# Les candidats n'ont tous pas la même chance d'être élu sur chaque ligne, les petits candidats "1,2,3 etc.."
-# sont plus souvent dans les simulations, et même vu que nous n'avons pas tous les mêmes options disponible
-# pour les candidats dans tout le dataframe, ça fausse tout, il faudrait faire ligne par ligne l'analyse
-
-
-
-# === Loading data ===
-library("readxl")
-df <- read_excel("experiments.xlsx")
-
-# === Prepare data ===
-
-df$Simu <- NULL # on enlève la variable inutile
-df$nVoters = factor(df$nVoters)
-#df$nVoters = NULL
-df$nCandidates = factor(df$nCandidates)
-#df$nCandidates = NULL
-df$typeSimu = factor(df$typeSimu)
-#df$typeSimu = NULL
-df$uninominal1T = factor(df$uninominal1T)
-df$uninominal2T = factor(df$uninominal2T)
-df$successif_elimination = factor(df$successif_elimination)
-df$bucklin = factor(df$bucklin)
-df$borda = factor(df$borda)
-df$nanson = factor(df$nanson)
-df$minimax = factor(df$minimax)
-df$copeland = factor(df$copeland)
-df$condorcet = factor(df$condorcet)
-df$range_voting = factor(df$range_voting)
-df$approval = factor(df$approval)
-df$majority_jugement = factor(df$majority_jugement)
-head(df)
-
-
-# === Analysis ===
-summary(df)
-library(FactoMineR)
-
-# === ACM ===
-
-# echantillon <- df[sample(nrow(df), 100), ]
-# res.mca <- MCA(echantillon)
-# couleurs <- c("red", "blue", "green")[as.numeric(factor(df$typeSimu))]
-# plot(res.mca,col.ind = couleurs, habillage = "typeSimu", title = "Nuage d'individus (Échantillon)")
-
-res.mca <- MCA(df)
-eig.val <- res.mca$eig # nuage individus
-barplot(eig.val[, 2],
-        names.arg = 1:nrow(eig.val),
-        main = "Variances Explained by Dimensions (%)",
-        xlab = "Principal Dimensions",
-        ylab = "Percentage of variances",
-        col ="steelblue")
-# Add connected line segments to the plot
-lines(x = 1:nrow(eig.val), eig.val[, 2],
-      type = "b", pch = 19, col = "red")
-
-plot(res.mca, autoLab = "yes") # => nuage individus + label groupe
-
-# === Heatmap ===
-library(ggplot2)
-
-similarity_matrix <- table(df$uninominal1T, df$uninominal2T)
-ggplot(data = as.data.frame(similarity_matrix)) +
-  geom_tile(aes(Var1, Var2, fill = Freq), colour = "white") +
-  scale_fill_gradient(low = "white", high = "steelblue") +
-  labs(x = "uninominal1T", y = "uninominal2T", title = "Graphique de proximité") +
-  theme_minimal()
-
-similarity_matrix <- table(df$majority_jugement, df$uninominal2T)
-ggplot(data = as.data.frame(similarity_matrix)) +
-  geom_tile(aes(Var1, Var2, fill = Freq), colour = "white") +
-  scale_fill_gradient(low = "white", high = "steelblue") +
-  labs(x = "majority_jugement", y = "uninominal2T", title = "Graphique de proximité") +
-  theme_minimal()
-
-
-# === CAH ===
-#res.hcpc = HCPC(res.mca)
-
-df$nVoters = NULL
-df$nCandidates = NULL
-df$typeSimu = NULL
-
-df$uninominal1T = as.numeric(factor(df$uninominal1T))
-df$uninominal2T = as.numeric(factor(df$uninominal2T))
-df$successif_elimination = as.numeric(factor(df$successif_elimination))
-df$bucklin = as.numeric(factor(df$bucklin))
-df$borda = as.numeric(factor(df$borda))
-df$nanson = as.numeric(factor(df$nanson))
-df$minimax = as.numeric(factor(df$minimax))
-df$copeland = as.numeric(factor(df$copeland))
-df$condorcet = as.numeric(factor(df$condorcet))
-df$range_voting = as.numeric(factor(df$range_voting))
-df$approval = as.numeric(factor(df$approval))
-df$majority_jugement = as.numeric(factor(df$majority_jugement))
-
-resultat_cah <- hclust(dist(t(df)))
-# Visualiser le dendrogramme résultant
-plot(resultat_cah, main = "Dendrogramme CAH")
-View(t(df))
-View(df)
-
-
+# # === Analysis ===
+# # ACM
+# # CAH
+# # MDS
+#
+# # ====
+#
+# # clustering entre les méthodes (en excluant les autres colonnes)
+# # ACM sur toute les variables ?
+# # mettre en facteur les resultats de méthodes ????
+#
+# # Les candidats n'ont tous pas la même chance d'être élu sur chaque ligne, les petits candidats "1,2,3 etc.."
+# # sont plus souvent dans les simulations, et même vu que nous n'avons pas tous les mêmes options disponible
+# # pour les candidats dans tout le dataframe, ça fausse tout, il faudrait faire ligne par ligne l'analyse
+#
+#
+#
+# # === Loading data ===
+# library("readxl")
+# df <- read_excel("experiments.xlsx")
+#
+# # === Prepare data ===
+#
+# # choose number of candidates
+# df <- df[df$nCandidates == 14,]
+#
+# df$Simu <- NULL # on enlève la variable inutile
+# df$nVoters = factor(df$nVoters)
+# #df$nVoters = NULL
+# df$nCandidates = factor(df$nCandidates)
+# #df$nCandidates = NULL
+# df$typeSimu = factor(df$typeSimu)
+# #df$typeSimu = NULL
+# df$uninominal1T = factor(df$uninominal1T)
+# df$uninominal2T = factor(df$uninominal2T)
+# df$successif_elimination = factor(df$successif_elimination)
+# df$bucklin = factor(df$bucklin)
+# df$borda = factor(df$borda)
+# df$nanson = factor(df$nanson)
+# df$minimax = factor(df$minimax)
+# df$copeland = factor(df$copeland)
+# df$condorcet = factor(df$condorcet)
+# df$range_voting = factor(df$range_voting)
+# df$approval = factor(df$approval)
+# df$majority_jugement = factor(df$majority_jugement)
+# head(df)
+#
+#
+# # === Analysis ===
+# summary(df)
+# library(FactoMineR)
+#
+# # === ACM ===
+#
+# # echantillon <- df[sample(nrow(df), 100), ]
+# # res.mca <- MCA(echantillon)
+# # couleurs <- c("red", "blue", "green")[as.numeric(factor(df$typeSimu))]
+# # plot(res.mca,col.ind = couleurs, habillage = "typeSimu", title = "Nuage d'individus (Échantillon)")
+#
+# res.mca <- MCA(df)
+# eig.val <- res.mca$eig # nuage individus
+# barplot(eig.val[, 2],
+#         names.arg = 1:nrow(eig.val),
+#         main = "Variances Explained by Dimensions (%)",
+#         xlab = "Principal Dimensions",
+#         ylab = "Percentage of variances",
+#         col ="steelblue")
+# # Add connected line segments to the plot
+# lines(x = 1:nrow(eig.val), eig.val[, 2],
+#       type = "b", pch = 19, col = "red")
+#
+# plot(res.mca, autoLab = "yes") # => nuage individus + label groupe
+#
+# # === Heatmap ===
+# library(ggplot2)
+#
+# similarity_matrix <- table(df$uninominal1T, df$uninominal2T)
+# ggplot(data = as.data.frame(similarity_matrix)) +
+#   geom_tile(aes(Var1, Var2, fill = Freq), colour = "white") +
+#   scale_fill_gradient(low = "white", high = "steelblue") +
+#   labs(x = "uninominal1T", y = "uninominal2T", title = "Graphique de proximité") +
+#   theme_minimal()
+#
+# similarity_matrix <- table(df$majority_jugement, df$uninominal2T)
+# ggplot(data = as.data.frame(similarity_matrix)) +
+#   geom_tile(aes(Var1, Var2, fill = Freq), colour = "white") +
+#   scale_fill_gradient(low = "white", high = "steelblue") +
+#   labs(x = "majority_jugement", y = "uninominal2T", title = "Graphique de proximité") +
+#   theme_minimal()
+#
+# similarity_matrix <- table(df$nanson, df$minimax)
+# ggplot(data = as.data.frame(similarity_matrix)) +
+#   geom_tile(aes(Var1, Var2, fill = Freq), colour = "white") +
+#   scale_fill_gradient(low = "white", high = "steelblue") +
+#   labs(x = "nanson", y = "minimax", title = "Graphique de proximité") +
+#   theme_minimal()
+#
+# # === CAH ===
+# #res.hcpc = HCPC(res.mca)
+#
+# df$nVoters = NULL
+# df$nCandidates = NULL
+# df$typeSimu = NULL
+#
+# df$uninominal1T = as.numeric(factor(df$uninominal1T))
+# df$uninominal2T = as.numeric(factor(df$uninominal2T))
+# df$successif_elimination = as.numeric(factor(df$successif_elimination))
+# df$bucklin = as.numeric(factor(df$bucklin))
+# df$borda = as.numeric(factor(df$borda))
+# df$nanson = as.numeric(factor(df$nanson))
+# df$minimax = as.numeric(factor(df$minimax))
+# df$copeland = as.numeric(factor(df$copeland))
+# df$condorcet = as.numeric(factor(df$condorcet))
+# df$range_voting = as.numeric(factor(df$range_voting))
+# df$approval = as.numeric(factor(df$approval))
+# df$majority_jugement = as.numeric(factor(df$majority_jugement))
+#
+# resultat_cah <- hclust(dist(t(df)))
+# # Visualiser le dendrogramme résultant
+# plot(resultat_cah, main = "Dendrogramme CAH")
+# View(t(df))
+# View(df)
+#
+# # === MDS ===
+#
+# resultat_mds <- cmdscale(dist(t(df)))
+# plot(resultat_mds, type = "n", xlab = "Dimension 1", ylab = "Dimension 2", main = "Analyse MDS")
+# text(resultat_mds, labels = rownames(df))
+#
+#
--- R/main.R
+++ R/main.R
@@ -10,13 +10,14 @@
 #' @export
 #' @import voteSim
 #' @import votingMethods
+#' @param n_candidates nC
 #' @param n_simulations number of simulations
 #' @returns dataframe
-experiments <- function(n_simulations = 10) {
+experiments <- function(n_candidates, n_simulations = 10) {
   start_time <- Sys.time()
   methods_names <- c("uninominal1T","uninominal2T","successif_elimination","bucklin","borda","nanson","minimax","copeland","condorcet","range_voting","approval","majority_jugement")
   simu_types <- c("generate_beta","generate_unif_continu","generate_norm")
-  n_candidates <- c(3,4,5,7,9,14) # OK
+  #n_candidates <- c(3,4,5,7,9,14) # OK
   n_voters <- c(9,15,21,51,101,1001,10001) # OK
 
   # Créer un data frame vide avec des colonnes renommées
@@ -30,51 +31,40 @@ experiments <- function(n_simulations = 10) {
     for (type in simu_types) {
       simulation <- get(type)
       # génère la situation maximale
-      situation <- simulation(max(n_voters),max(n_candidates)) # on génére le max puis on prends des samples
-
+      situation <- simulation(max(n_voters),n_candidates) # on génére le max puis on prends des samples
       # =======
-
       first_n_voter <- n_voters[1]
-      first_n_candidate <- n_candidates[1]
       echantillon1_voter <- sample(ncol(situation), first_n_voter, replace = FALSE) # init
-      echantillon1_candidate <- sample(nrow(situation),first_n_candidate, replace = FALSE) # init
       for(voter in n_voters){
         colonnes_supplementaires <- sample(setdiff(1:ncol(situation), echantillon1_voter),voter-length(echantillon1_voter), replace = FALSE)
         echantillon_final_voter <- c(echantillon1_voter,colonnes_supplementaires)
-        for(candidate in n_candidates){
-          lignes_supplementaires <- sample(setdiff(1:nrow(situation), echantillon1_candidate),candidate-length(echantillon1_candidate), replace = FALSE)
-          echantillon_final_candidate <- c(echantillon1_candidate, lignes_supplementaires)
-          condorcet <- "None"
-          winners <- c()
-
-          # =======
-
-          for(method in methods_names){
-            if(method == "uninominal1T"){
-              scrutin <- get("uninominal")
-              winner <- scrutin(situation[echantillon_final_candidate,echantillon_final_voter])
-            }else if(method == "uninominal2T"){
-              scrutin <- get("uninominal")
-              winner <- scrutin(situation[echantillon_final_candidate,echantillon_final_voter],2)
-            }else if(method == "copeland"){
-              scrutin <- get("copeland")
-              winner <- scrutin(situation[echantillon_final_candidate,echantillon_final_voter])
-              condorcet <- winner$condorcet
-              winner <- winner$copeland
-              # ajouter colonne condorcet
-            }else if(method == "condorcet"){
-              winner <- condorcet
-            }
-            else{
-              scrutin <- get(method)
-              winner <- scrutin(situation[echantillon_final_candidate,echantillon_final_voter])
-            }
-            winners <- c(winners,winner) # on ajoute le gagnant de chaque méthodes
+        condorcet <- "None"
+        winners <- c()
+        # =======
+        for(method in methods_names){
+          if(method == "uninominal1T"){
+            scrutin <- get("uninominal")
+            winner <- scrutin(situation[,echantillon_final_voter])
+          }else if(method == "uninominal2T"){
+            scrutin <- get("uninominal")
+            winner <- scrutin(situation[,echantillon_final_voter],2)
+          }else if(method == "copeland"){
+            scrutin <- get("copeland")
+            winner <- scrutin(situation[,echantillon_final_voter])
+            condorcet <- winner$condorcet
+            winner <- winner$copeland
+          }else if(method == "condorcet"){
+            winner <- condorcet
           }
-          nouvelle_ligne <- c(n, voter, candidate, type, winners)
-          df[cpt,] <- nouvelle_ligne # OU df <- rbind(df, nouvelle_ligne) (rbind enlève les colonnes, pas ouf)
-          cpt <- cpt +1
+          else{
+            scrutin <- get(method)
+            winner <- scrutin(situation[,echantillon_final_voter])
+          }
+          winners <- c(winners,winner) # on ajoute le gagnant de chaque méthodes
         }
+        nouvelle_ligne <- c(n, voter, n_candidates, type, winners)
+        df[cpt,] <- nouvelle_ligne
+        cpt <- cpt +1
       }
     }
   }
@@ -83,7 +73,7 @@ experiments <- function(n_simulations = 10) {
   end_time <- Sys.time() - start_time
   print("execution time : ")
   print(end_time)
-  # export_experiments_to_excel(df)
+  export_experiments_to_excel(df)
   return(df)
 }
 
