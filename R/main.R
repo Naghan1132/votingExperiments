ff36b0f0c005cf2d9b4dc9da5ab9053b4f99f137
Naghan1132 <grimault52@gmail.com>
2023-06-15 11:12:39 +0200

    change value alpha/beta for experiments

--- R/main.R
+++ R/main.R
@@ -243,8 +243,8 @@ evolving_alpha_beta <- function(n_simulations){
   start_t <- Sys.time()
   n_voters <- c(15)
   n_candidates <- c(3) # OK
-  alpha <- c(0.2,0.6)
-  beta <- c(0.1,0.3)
+  alpha <- c(0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95,1)
+  beta <- c(0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95,1)
 
   # ==== Boucle de simulations ====
   for (a in alpha) {
